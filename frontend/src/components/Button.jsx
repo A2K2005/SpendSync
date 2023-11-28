940dc5f29984e3027f734f2441ff1cd978f0e700
A2K <82535890+A2K2005@users.noreply.github.com>
2023-11-28 21:58:56 +0530

    Update Button.jsx

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,9 @@ const Button = ({ styles }) => (
     type="button"
     className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary1 bg-blue-gradient rounded-[10px] outline-none ${styles}`}
   >
+  <a href="https://spend-sync-5dmp.vercel.app/" target="_blank">
     Get Started
+    </a>
   </button>
 );
 
